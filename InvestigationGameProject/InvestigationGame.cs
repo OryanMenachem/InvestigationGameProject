bc7806a24901584ba624db7b86937e3cd0aa25ca
Oryan Menachem <om3160204@gmail.com>
2025-06-19 16:26:57 +0300

    Add project files.

---
+++ InvestigationGameProject/InvestigationGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestigationGameProject
+{
+    internal class InvestigationGame
+    {
+        public void Start() { }
+
+        private static void DisplayGeneralMessageAboutGame() { }
+
+        private static void DisplayMenu() { }
+
+        private void HandlingUserSelection() { }
+
+        private void StartNewGame() { }
+
+        private static void DisplayGameInstructions() { }
+
+        private void ExitTheGame() { }
+
+        
+            
+     
+    }
+}
