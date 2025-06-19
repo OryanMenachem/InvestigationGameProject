eedbaa6fbaae6bcf68cb362f92bae64d68a75132
Oryan Menachem <om3160204@gmail.com>
2025-06-19 17:03:17 +0300

    Implementing the 'Investigation Game' class

--- InvestigationGameProject/InvestigationGame.cs
+++ InvestigationGameProject/InvestigationGame.cs
@@ -8,22 +8,98 @@ namespace InvestigationGameProject
 {
     internal class InvestigationGame
     {
-        public void Start() { }
+        private bool Flag = true;
+        public void Start() 
+        {
+            DisplayGeneralMessageAboutGame();
 
-        private static void DisplayGeneralMessageAboutGame() { }
+            while (Flag)
+            {
+                DisplayMenu();
 
-        private static void DisplayMenu() { }
+                HandlingUserSelection(ConsoleDesign.Input());
 
-        private void HandlingUserSelection() { }
+            }
+
+                
+        }
+
+        private static void DisplayGeneralMessageAboutGame() 
+        {
+
+            Console.Write("Welcome to the ");
+            ConsoleDesign.CyanColor(" * INVESTIGATION GAME * ", false);
+            Console.WriteLine("A game where you expose Iranian agents using sensors.\n");
+
+
+        }
+
+        private static void DisplayMenu() 
+        {
+
+            ConsoleDesign.CyanColor("Please select one of the following options:\n");
+
+            ConsoleDesign.CyanColor("\n******* MENU **********");
+            ConsoleDesign.CyanColor("\n* 1. new game.        *");
+            ConsoleDesign.CyanColor("\n* 2. instructions     *");
+            ConsoleDesign.CyanColor("\n* 3. exit.            *");
+            ConsoleDesign.CyanColor("\n***********************");
+        }
+
+        private void HandlingUserSelection(string choice) 
+        {
+    
+
+            switch (choice)
+            {
+                case "1":
+                    StartNewGame();
+                    break;
+                case "2":
+                    DisplayGameInstructions();
+                    break;
+                case "3":
+                    ExitTheGame();
+                    break;
+                default:
+                    DisplayMessageWrongChoice();
+                    break;
+
+            }
+        }
 
         private void StartNewGame() { }
 
-        private static void DisplayGameInstructions() { }
+        private static void DisplayGameInstructions() 
+        {
+            ConsoleDesign.CyanColor("Instructions:\n");
+            Console.WriteLine("- You will now interrogate an Iranian agent of a random rank.\n" +
+                              "- Your mission is to uncover which sensors are needed to expose him.\n" +
+                              "- Note that the higher the rank of the agent, the more sensors are needed to expose him.\n");
+            ConsoleDesign.CyanColor("Good luck!\n");
 
-        private void ExitTheGame() { }
+            Console.WriteLine("To return to the menu, press enter...\n");
+            Console.ReadLine();
+            Console.Clear();
+        }
 
-        
-            
-     
+        private void ExitTheGame() 
+        {
+            Console.Clear();
+            Console.WriteLine();
+            ConsoleDesign.CyanColor("Good bye!");
+            Flag = false;
+        }
+
+        private static void DisplayMessageWrongChoice()
+        {
+            ConsoleDesign.ErrorColor("No valid choice was made!\n");
+        }
+
+
+
+
+
+
     }
 }
