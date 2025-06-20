aba512d454e170d1d243300c39a4f7971e3f29e5
Oryan Menachem <om3160204@gmail.com>
2025-06-20 14:49:19 +0300

    Implementing the 'BaseAgent' class, a parent class for all agent types. and implementing the 'FootSoldier' class.

--- InvestigationGameProject/AgentsF/FootSoldier.cs
+++ InvestigationGameProject/AgentsF/FootSoldier.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InvestigationGameProject.AgentsF;
 
 namespace InvestigationGameProject.GameF
 {
-    internal class FootSoldier
+    internal class FootSoldier : BaseAgent
     {
+        public override string AgentName { get; set; }
+        public override string AgentRank { get; set; } = "foot soldier";
+
+        public override List<string> SensitiveToSensors { get; set; } = new List<string> {Sensortypes[0], Sensortypes[0] };
+
+        public override List<string> AttachedSensors { get; set; } = new List<string> {null, null };
+
+
     }
 }
